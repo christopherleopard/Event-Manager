a5fcbe4c46710d7014975d109e35e3186b3f0bd8
alina-gasperino <alinacreatesthings@gmail.com>
2025-07-25 14:07:16 -0400

    default admin user

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+AdminUser.create!(email: 'mpetriella@ignitexds.com', password: 'mpetriella123', password_confirmation: 'mpetriella123')
 NewsletterSetting.first_or_create
 FoodService::SCHOOLS.each do |school|
   FoodService.find_or_create_by!(school: school)
