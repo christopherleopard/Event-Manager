6d97891642dc7a2a5dfe7d8ed2a6c694f2a15aeb
alina-gasperino <alinacreatesthings@gmail.com>
2025-07-28 10:48:26 -0400

    custom header

--- config/initializers/active_admin_override_header.rb
+++ config/initializers/active_admin_override_header.rb
@@ -1,3 +1,7 @@
-Rails.application.config.to_prepare do
-  load Rails.root.join("app/overrides/active_admin/custom_header.rb")
+if Rails.env.production?
+  require Rails.root.join("overrides/active_admin/custom_header.rb")
+else
+  Rails.application.config.to_prepare do
+    load Rails.root.join("app/overrides/active_admin/custom_header.rb")
+  end
 end
