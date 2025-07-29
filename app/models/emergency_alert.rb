b91a4908f95e764dbc0849c5e1b35faf43710970
alina-gasperino <alinacreatesthings@gmail.com>
2025-07-29 12:22:32 -0400

    Update Emergency Alert to have only one active

--- app/models/emergency_alert.rb
+++ app/models/emergency_alert.rb
@@ -2,11 +2,21 @@ class EmergencyAlert < ApplicationRecord
   validates :name, presence: true
   validates :message, presence: true
 
-  def self.ransackable_attributes(auth_object = nil)
-    [ "created_at", "enabled", "id", "message", "name", "updated_at" ]
+  before_save :disable_other_alerts, if: :should_disable_others?
+
+  scope :active, -> { where(enabled: true) }
+
+  def self.ransackable_attributes(_auth_object = nil)
+    %w[created_at enabled id message name updated_at]
   end
 
-  def self.active
-    where(enabled: true)
+  private
+
+  def should_disable_others?
+    enabled_changed? && enabled?
+  end
+
+  def disable_other_alerts
+    EmergencyAlert.where.not(id: id).update_all(enabled: false)
   end
 end
